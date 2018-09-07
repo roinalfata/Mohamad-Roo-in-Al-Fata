8d400bcb7c4723c1fe31ef4bc18fff997cb8d512
roinalfata <roinalfa32@gmail.com>
2018-09-07 09:09:02 +0700

    Tugas 1

---
+++ src/rooin/Rooin.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rooin;
+
+/**
+ *
+ * @author Nuser
+ */
+public class Rooin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    int jumlah=5;
+            for (int i=1;i<=jumlah;i++){
+                System.out.println("");
+                        for (int j=1;j<=i;j++){
+                            System.out.print("X");
+                        }
+            }}
+    
+}
